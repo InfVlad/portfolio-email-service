ba44924055646a4cf2012908bc58833b1097fbcc
Vladimir Infante <infantevladimir@gmail.com>
2024-09-05 15:26:26 -0500

    feat: added error handler middleware

---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,18 @@
+import type { Context } from "hono";
+
+export const handleErrorMiddleware = (c: Context, error: any) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+
+  // Log the error details (optional)
+  if (process.env.DEBUG === "true")
+    console.error(`[ERROR] ${message}`, error.statusCode, error.details);
+
+  return c.json(
+    {
+      message,
+      error: error.details || null,
+    },
+    statusCode
+  );
+};
