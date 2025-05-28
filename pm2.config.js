92769c414b932f661ef661e096de37cfeba8f4be
Vladimir Infante <infantevladimir@gmail.com>
2025-05-28 15:17:41 -0500

    fix: wrong config on pm2 args

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "portfolio-mailer",
       script: "./dist/index.js",
       interpreter: process.env.INTERPRETER,
-      args: ["run"],
+      args: "run",
       watch: false,
       max_memory_restart: "200M",
     },
